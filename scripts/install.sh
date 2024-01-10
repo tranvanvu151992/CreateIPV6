943bab1c307e856ccaea47bddbe146ce5264f34b
Mr Trần Vũ RB <tranvanvu151992@gmail.com>
2024-01-10 09:23:27 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,7 +13,7 @@ gen64() {
 }
 install_3proxy() {
   echo "installing 3proxy"
-  URL="https://github.com/tranvanvu151992/3proxy/blob/main/archive.tar.gz"
+  URL="https://github.com/3proxy/3proxy"
   wget -qO- $URL | bsdtar -xvf-
   cd 3proxy-3proxy-0.8.6
   make -f Makefile.Linux
