84a3beb1d66d98b6edb0f2d1d3410e1f4153d718
Mr Trần Vũ RB <tranvanvu151992@gmail.com>
2024-01-11 16:03:47 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
-
 random() {
   tr </dev/urandom -dc A-Za-z0-9 | head -c5
   echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-
 gen64() {
   ip64() {
     echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
   }
   echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
-
 install_3proxy() {
-  echo "Installing 3proxy..."
+  echo "installing 3proxy"
   URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
   wget -qO- $URL | bsdtar -xvf-
   cd 3proxy-3proxy-0.8.6
@@ -37,28 +34,34 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth none
-allow *
+auth strong
 
-$(awk -F "/" '{print "proxy -6 -n -a -p" $2 " -i" $1 " -e"$3"\n" \
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
 
 gen_proxy_file_for_user() {
   cat >proxy.txt <<EOF
-$(awk -F "/" '{print $1 ":" $2 }' ${WORKDATA})
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
 upload_proxy() {
-  local PASS=$(random)
-  zip --password $PASS proxy.zip proxy.txt
-  URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+  #local PASS=$(random)
+  #zip --password $PASS proxy.zip proxy.txt
+  #URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
 
-  echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-  echo "Download zip archive from: ${URL}"
-  echo "Password: ${PASS}"
+  #echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+  #echo "Download zip archive from: ${URL}"
+  #echo "Password: ${PASS}"
+  
+  sed -n '1,1000p' proxy.txt
+
 }
 
 install_jq() {
@@ -77,53 +80,52 @@ upload_2file() {
 
 gen_data() {
   seq $FIRST_PORT $LAST_PORT | while read port; do
-    echo "$IP4/$port/$(gen64 $IP6)"
+    echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
   done
 }
 
 gen_iptables() {
   cat <<EOF
-$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $1 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
 EOF
 }
 
 gen_ifconfig() {
   cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $3 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
-
-echo "Installing necessary apps..."
+echo "installing apps"
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-echo "Working folder: /home/proxy-installer"
+echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $WORKDIR
+mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal IP: ${IP4}. External subnet for IP6: ${IP6}"
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-echo "How many proxies do you want to create? Example: 500"
+echo "How many proxy do you want to create? Example 500"
 read COUNT
 
-FIRST_PORT=10000
-LAST_PORT=$(($FIRST_PORT + $COUNT))
+FIRST_PORT=10001
+LAST_PORT=$((($FIRST_PORT + $COUNT)-1))
 
-gen_data >$WORKDATA
+gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x $WORKDIR/boot_*.sh /etc/rc.local
+chmod +x boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
-bash $WORKDIR/boot_iptables.sh
-bash $WORKDIR/boot_ifconfig.sh
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 service 3proxy start
 EOF
